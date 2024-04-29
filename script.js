42ae41de615d6f29dd92607535bb91b6e2ac39c7
IntCarlosI <168407179+IntCarlosI@users.noreply.github.com>
2024-04-29 03:13:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+        $(document).ready(function(){
+            $('.carousel').slick({
+                autoplay: true, // Habilita la reproducción automática
+                autoplaySpeed: 2000, // Velocidad de reproducción automática en milisegundos
+                arrows: true, // Muestra flechas de navegación
+                dots: true // Muestra indicadores de puntos
+            });
+        });
+ 
